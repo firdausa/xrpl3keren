550876fe5ae351c147e7e080fcb4b8e9083f9505
Firdausa <firdausa@smktelkom-mlg.sch.id>
2024-02-15 14:07:15 +0700

    add file kalkulator.java

---
+++ kalkulator.java
@@ -0,0 +1,68 @@
+private void bt_0ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 0
+        bilangan +="0";        
+        txt_hasil.setText(bilangan);
+} 
+ 
+private void bt_1ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 1
+        bilangan +="1";        
+        txt_hasil.setText(bilangan);
+}   
+ 
+private void bt_2ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 2
+        bilangan +="2";        
+        txt_hasil.setText(bilangan);
+    }
+ 
+private void bt_3ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 3
+        bilangan +="3";        
+        txt_hasil.setText(bilangan);
+    }                                    
+ 
+    private void bt_4ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 4
+        bilangan +="4";        
+        txt_hasil.setText(bilangan);
+    }                                    
+ 
+    private void bt_5ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 5
+        bilangan +="5";        
+        txt_hasil.setText(bilangan);
+    }                                    
+ 
+    private void bt_6ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 6
+        bilangan +="6";        
+        txt_hasil.setText(bilangan);
+    }                                    
+ 
+    private void bt_7ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 7
+        bilangan +="7";        
+        txt_hasil.setText(bilangan);
+    }                                    
+ 
+    private void bt_8ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 8
+        bilangan +="8";        
+        txt_hasil.setText(bilangan);
+    }                                    
+ 
+    private void bt_9ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // fungsi untuk tombol 9
+        bilangan +="9";        
+        txt_hasil.setText(bilangan);
+    }
+ 
+private void bt_clearActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        // fungsi untuk tombol c
+        txt_hasil.setText(null);
+        bil1=0.0;
+        bil2=0.0;
+        jumlah=0.0;
+        bilangan="";
+    }
